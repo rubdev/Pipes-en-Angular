11db30c60842208ead8453c3dd6e0a66d0367ce6
Rubén Segura <rsegrom@gmail.com>
2019-07-10 18:49:52 +0200

    Agregados nuevos pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Rubén';
+  tabla = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  porcentaje = 0.54723434534;
+  pokemon = {
+    nombre: 'pikachu',
+    tipo: 'electrico',
+    evoluciona: 'si'
+  };
+  hoy = Date();
 }
